6dc8055aa7bf1486bc94a377737290654321df41
Leonardo Merza <ljmerza@gmail.com>
2019-01-26 12:21:43 -0500

    automated commit 26/01/2019 12:21:43

---
+++ src/example_config.h
@@ -0,0 +1,18 @@
+
+#define IsFahrenheit true
+
+#define wifi_ssid "YourSSID"
+#define wifi_password "YourWIFIpassword"
+#define mqtt_server "your.mqtt.server.ip"
+#define mqtt_user "yourMQTTusername" 
+#define mqtt_password "yourMQTTpassword"
+#define mqtt_port 1883
+
+#define light_state_topic "multisensor/sensornode1"
+
+#define SENSORNAME "sensornode1"
+#define OTApassword "YouPassword"
+
+#define PIRPIN D5
+#define DHTPIN D7
+#define DHTTYPE DHT22
